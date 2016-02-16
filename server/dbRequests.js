da75d393167407044eda4775e9ffe39c5b274fe7
Julia Brown <juliacelestebrown@gmail.com>
2016-02-16 16:30:21 -0600

    outsource db call logic to new file some progress on writing lookup functions

---
+++ server/dbRequests.js
@@ -0,0 +1,76 @@
+require('./db');
+var knex = require('knex');
+
+var dbRequest = module.exports
+
+dbRequest.lookupWork = function(req){
+	var title = req.title //or whatever that path ends up being
+	    var type = req.type //same as above
+
+	    knex.from(type).where('title', title) //maybe change this to a LIKE to account for case errors or something?
+	        .then(function(result){
+	          //add result to response body and return
+	          return result;
+	        })
+	        .catch(function(err){
+	          //if err.message.match() something indicating it wasn't found? or just always make query to api
+	          //make call to appropriate api and return that in response
+	          //also should make POST req to api/works to add it into the database
+	        })
+	    //response should have an array of the searched for object in it.
+};
+
+dbRequest.addWork = function(req){
+
+};
+
+dbRequest.findWorks = function(req){
+	var tagsArr = req.tags // => must be array
+
+    //returns all tag ids for the passed in tags
+    var tagLookup = knex.select('id').from('Tags').whereIn('tag', tagsArr);
+    //returns work ids for all works that have any of the given tags
+    var workLookup = knex.select('work_id').from('TagWork').whereIn('tag_id', tagLookup);
+
+    //inner join of books, games, movies tables to return all colomns for each work_id
+    knex.from(/* all work tables*/).whereIn('id', workLookup)
+        .then(function(results){
+          //put results into response body and send off
+          //I think this will return all works that match at least one tag
+        })
+        .catch(function(error){
+          //won't get no matching works unless we filter out the searched for thing
+        })
+};
+
+dbRequest.findTags = funciton(req){
+	
+}
+
+
+dbRequest.addTags = function(req){
+	var title = req.title; // => should be a string of a single work
+    var tagNames = req.tags; // => must be an array
+
+    //finds id for the given title
+    knex.select('id').from('Works').where('title', title)
+        .then(function(row){
+          return row[0].id
+        })
+        .then(function(workId){
+          //finds id for given tags
+          knex.select('id').from('Tags').where('tag', tagName)
+              .map(function(row){
+                return row.id;
+              })
+              .then(function(tagIds){
+                //tagIds is an array of the selected tags ids
+                console.log('tagIds after map promise ', tagIds);
+                //now we need to put tagIds and workId together into array of objects
+                return tagIds
+              })
+        })
+    
+    knex.insert(newTags).into('WorkTag')
+
+};
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var Path = require('path');
 var routes = express.Router();
 require('./db');
-var knex = require('knex')
+var db = ('./dbRequests.js');
 //
 //route to your index.html
 //
@@ -16,30 +16,67 @@ routes.get('/api/tags-example', function(req, res) {
   res.send(['node', 'express', 'angular'])
 });
 
+//if we are in development or production mode
 if(process.env.NODE_ENV !== 'test') {
-  
+
+//GET api/works --> looks to see if we have the given work already in the database
   routes.get('/api/works', function(req, res){
-    //look into request body and see what contents are
-    //response should have an array of objects inside of res.body.results
+    db.lookupWork(req.body)
+      .then(function(result){
+        //puts result into response
+      })
+      .catch(function(error){
+        //if error.message = 'Not found in database' --> make call to api --> confirm with user --> POST to api/works
+        //else 500 server error
+        console.error('error in GET to api/works ', err)
+      })
   })
 
+//POST api/works --> gets called if the work doesn't exist in database already AFTER api lookup has been done
   routes.post('/api/works', function(req, res){
     //look in request body
-    //first check if that work already exists
-    //does a knex add to the db of the given work
+    //does a knex insert to the db of the given work
+    db.addWork(req.body)
+      .then(function(result){
+
+      })
+      .catch(function(err){
+        console.error('error in POST to api/works ', err);
+        //probably 500 server error or it didn't have everything it needed
+      })
   })
 
+  //GET api/tags --> looks for all works with passed in tag names --> results will include the looked for work
   routes.get('/api/tags', function(req, res){
-    //return res.body.results of all works that have the given tags
-    var search = req.body //whatever this path ends up being to the tags themselves
-    knex('tagsjoin').where({  //a join table lookup???
-      tag: 
-    })
+    db.findWorks(req.body)
+      .then(function(result){
+
+      })
+      .catch(function(err){
+        console.error('error in GET to api/tags ', err);
+      })
+    
   })
 
+  //POST api/tags --> if tags need to be added to a given work
   routes.post('/api/tags', function(req, res){
-    //check if tags already exist for given work
-    //if not add to the db
+    //first look to see what tags should be added
+    db.findTags(req.body)
+      .then(function(newTags){
+        //then run function that adds the new tags
+        db.addTags(req.body, newTags) //title that tags should be added to and ONLY the new tags 
+          .then(function(result){
+            //return 201 
+          })
+          .catch(function(error){
+            //some sort of error
+            console.error('error in POST to api/tags inside addTags ', err)
+          })
+      })
+      .catch(function(error){
+        console.error('error in POST to api/tags inside findTags ', err)
+      })
+
   })
 
 
