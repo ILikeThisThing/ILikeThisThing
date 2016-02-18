b8d07a5786a3206961a3e901abfd624be35bd997
Natalie <natalie.s.sharpe@gmail.com>
2016-02-18 17:35:14 -0600

    initial code for GET request controller added to homepage.js

--- client/homepage.js
+++ client/homepage.js
@@ -1,4 +1,16 @@
 var app=angular.module('myApp', [ ]);
 
-// app.controller('SiteController', function(){
-// })
+// following controller code taken from Angular documentation on $http service
+// leaving here to fiddle with soon
+app.controller('SiteController', function(){
+	$http({
+  method: 'GET',
+  url: '/someUrl'
+}).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+})
