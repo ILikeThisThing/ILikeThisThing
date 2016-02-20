f32a378c90804d51d9d2302713ce3c293be0a6a3
Natalie <natalie.s.sharpe@gmail.com>
2016-02-20 15:39:53 -0600

    deleted homepage files

--- client/index.js
+++ client/index.js
@@ -1,16 +1,38 @@
 var app=angular.module('myApp', [ ]);
 
-// 
+/*
+app.controller('RequestController', function) {
+// create object out of user input -
+// containing two key value pairs - for title
+// and type - this object will then be passed
+// into the UserRequest factory function
+var submission = {};
+
+}
+
+
+
 .factory('UserRequest', function ($http) {
   var getMatches = function () {
     return $http({
-      method: 'GET',
-      url: '/server/dbrequests'
+      method: 'POST',
+      url: '/api/searchworks'
     })
     .then(function (resp) {
       return resp.data;
     });
   };
+
+*/
+
+
+
+
+
+
+
+
+
 
 
 //http interceptor to work with :
@@ -18,25 +40,27 @@ var app=angular.module('myApp', [ ]);
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
-    $httpProvider.interceptors.push('AlreadyIncludedCheck');
-})
-.factory('AlreadyIncludedCheck', function ($window) {
+//     $httpProvider.interceptors.push('AlreadyIncludedCheck');
+// })
+// .factory('AlreadyIncludedCheck', function ($window) {
   // this is an $httpInterceptor
   // its job is to stop all out going request
   // then look in local storage and find the user's token
   // then add it to the header so the server can validate the request
-  var attach = {
-    request: function (object) {
-      var jwt = $window.localStorage.getItem('com.shortly');
-      if (jwt) {
-        object.headers['x-access-token'] = jwt;
-      }
-      object.headers['Allow-Control-Allow-Origin'] = '*';
-      return object;
-    }
-  };
-  return attach;
-})
+  
+  // send it into lookupWork
+//   var checkDB = {
+//     request: function (object) {
+//       var gotIt = $window.localStorage.getItem('com.shortly');
+//       if (gotIt) {
+//         object.headers['x-access-token'] = gotIt;
+//       }
+//       object.headers['Allow-Control-Allow-Origin'] = '*';
+//       return object;
+//     }
+//   };
+//   return checkDB;
+// })
 
 
 
@@ -44,15 +68,15 @@ var app=angular.module('myApp', [ ]);
 
 // following controller code taken from Angular documentation on $http service
 // leaving here to fiddle with soon
-app.controller('SiteController', function(){
-	$http({
-  method: 'GET',
-  url: '/someUrl'
-}).then(function successCallback(response) {
+// app.controller('SiteController', function(){
+//  $http({
+//   method: 'GET',
+//   url: '/someUrl'
+// }).then(function successCallback(response) {
     // this callback will be called asynchronously
     // when the response is available
-  }, function errorCallback(response) {
+  // }, function errorCallback(response) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
-  });
-})
+//   });
+// })
