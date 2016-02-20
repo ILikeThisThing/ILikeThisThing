684649750b9dc05a82e0c9ee97464a109133db46
Julia Brown <juliacelestebrown@gmail.com>
2016-02-20 14:49:32 -0600

    add tag seed and knexfile update

---
+++ seeds/tags.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('Tags').del(), 
+
+    // Inserts seed entries
+    knex('Tags').insert([{tag: 'Dark/macabre'}, {tag: 'Absurdist'}, {tag: 'Existential'},
+    	{tag: 'Happy-Go-Lucky'}, {tag: 'Dystopian'}, {tag: 'Utopian'}, {tag: 'Beauty/Wonderment'},
+    	{tag: 'Weird'}, {tag: 'Goofy'}, {tag: 'Stream of Consciousness'}, {tag: 'Vignette'},
+    	{tag: 'Present Day'}, {tag: 'Victorian era'}, {tag: 'Medieval times'}, {tag: 'Prehistoric times'}, {tag: 'Action'},
+    	{tag: 'Adventure'}, {tag: 'Comedy'}, {tag: 'Crime'}, {tag: 'Fantasy'}, {tag: 'Horror'}, {tag: 'Musical'}, {tag: 'Mystery'},
+    	{tag: 'Political'}, {tag: 'Romance'}, {tag: 'Satire'}, {tag: 'Science Fiction'}, {tag: 'Thriller'}, {tag: 'Western'}, {tag: 'So-Bad-its-Good'}]),
+  );
+};
