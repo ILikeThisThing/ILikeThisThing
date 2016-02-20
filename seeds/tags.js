4e47cebe0af6395c80daa0478c55fcb52a61cb8f
Julia Brown <juliacelestebrown@gmail.com>
2016-02-20 15:52:45 -0600

    seeds actually work now

--- seeds/tags.js
+++ seeds/tags.js
@@ -9,7 +9,8 @@ exports.seed = function(knex, Promise) {
     	{tag: 'Happy-Go-Lucky'}, {tag: 'Dystopian'}, {tag: 'Utopian'}, {tag: 'Beauty/Wonderment'},
     	{tag: 'Weird'}, {tag: 'Goofy'}, {tag: 'Stream of Consciousness'}, {tag: 'Vignette'},
     	{tag: 'Present Day'}, {tag: 'Victorian era'}, {tag: 'Medieval times'}, {tag: 'Prehistoric times'}, {tag: 'Action'},
-    	{tag: 'Adventure'}, {tag: 'Comedy'}, {tag: 'Crime'}, {tag: 'Fantasy'}, {tag: 'Horror'}, {tag: 'Musical'}, {tag: 'Mystery'},
-    	{tag: 'Political'}, {tag: 'Romance'}, {tag: 'Satire'}, {tag: 'Science Fiction'}, {tag: 'Thriller'}, {tag: 'Western'}, {tag: 'So-Bad-its-Good'}]),
-  );
+    	{tag: 'Adventure'}, {tag: 'Comedy'}, {tag: 'Crime'}, {tag: 'Fantasy'}, {tag: 'Horror'}, {tag: 'Musical'}, 
+    	{tag: 'Mystery'}, {tag: 'Political'}, {tag: 'Romance'}, {tag: 'Satire'}, {tag: 'Science Fiction'}, 
+    	{tag: 'Thriller'}, {tag: 'Western'}, {tag: 'So-Bad-its-Good'}])
+  )
 };
