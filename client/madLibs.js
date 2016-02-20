164491c79df65ca352384d0f8e5c7ba9b60071bc
Brad Fincher <matrixv01@gmail.com>
2016-02-20 17:33:32 -0600

    add factory templates(madlibs,recs)

--- client/madLibs.js
+++ client/madLibs.js
@@ -2,10 +2,22 @@ angular.module('madLibs', [])
 
 .controller('RecController', function($scope, $http) {
   $http.get('api/works')
-  .success(function(data) {
-    $scope.works = data;
-  })
-  .error(function(data) {
-    console.error(error);
-  });
+    .success(function(data) {
+      $scope.works = data;
+    })
+    .error(function(data) {
+      console.error(error);
+    });
+})
+
+.factory('Tags', function ($http) {
+  var getAll = function () {
+    return $http({
+      method: 'POST',
+      url: '/api/works'
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
+  };
 });
--- client/recommendations.js
+++ client/recommendations.js
@@ -1,11 +1,23 @@
 angular.module('recommendations', [])
 
 .controller('RecController', function($scope, $http) {
-  $http.get('data/recs.json')
-  .success(function(data) {
-    $scope.recs = data;
-  })
-  .error(function(data) {
-    console.error(error);
-  });
+  $http.get('api/works')
+    .success(function(data) {
+      $scope.recs = data;
+    })
+    .error(function(data) {
+      console.error(error);
+    });
+})
+
+.factory('Recs', function ($http) {
+  var getAll = function () {
+    return $http({
+      method: 'POST',
+      url: '/api/works'
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
+  };
 });
