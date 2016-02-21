aefc0a788e440fb4f9e5f5e428bb5bc75a213327
Natalie <natalie.s.sharpe@gmail.com>
2016-02-21 15:23:46 -0600

    work on displaying POST response

--- client/index.js
+++ client/index.js
@@ -2,12 +2,6 @@ var app=angular.module('myApp', [ ]);
 
 
 app.controller('RequestController', function($scope, $http) {
-// create object out of user input -
-// containing two key value pairs - for title
-// and type - this object will then be passed
-// into the UserRequest factory function
-
-// $scope.title = 'The Room';
 
 //different types to populate the dropdown menu
 $scope.types = ['Books', 'Movies', 'Games'];
@@ -18,16 +12,11 @@ $scope.userInput = {
   type: ''
 };
 
-// $scope.saveData = function() {
-//   console.log($scope.userInput);
-// };
-
 $scope.submitForm = function() {
   return $http({
     method: 'POST',
     url: '/api/searchworks',
     data: $scope.userInput, //forms user object
-    // headers: {'Content-Type': 'application/x-www-form-urlencoded'}
   })
   .then(function successCallback(response) {
     // this callback will be called asynchronously
@@ -35,6 +24,50 @@ $scope.submitForm = function() {
     console.log(response);
     var data = response.data;
     return data;
+    console.log(data);
+
+      if ... database && $scope.data.type === 'Movies'
+        $scope.data.title = title;
+        $scope.data.poster = image;
+        $scope.data.director = person;
+
+      alternate for !database && $scope.data.type === 'Movies'
+        $scope.data.title = title;
+        $scope.data.poster = image;
+        $scope.data.director = person;
+
+      if ... database && $scope.data.type === 'Books'
+        $scope.data.title = title;
+        $scope.data.poster = image;
+        $scope.data.director = person;
+
+      alternate for !database && $scope.data.type === 'Books'
+        $scope.data.title = title;
+        $scope.data.poster = image;
+        $scope.data.director = person;
+
+      if ... database && $scope.data.type === 'Games'
+        $scope.data.title = title;
+        $scope.data.poster = image;
+        $scope.data.director = person;
+
+      alternate for !database && $scope.data.type === 'Games'
+
+        $scope.data.title = title;
+        $scope.data.poster = image;
+        $scope.data.director = person;
+
+
+
+
+
+
+
+
+
+
+
+
 
   }, function errorCallback(response) {
     // called asynchronously if an error occurs
@@ -50,98 +83,4 @@ $scope.submitForm = function() {
 //     restrict: 'E',
 //     templateUrl: 'initialResults.html'
 //   }
-// })
-
-
-
-
-// $scope.displayResults =
-    
-
-// var signin = function (user) {
-//     return $http({
-//       method: 'POST',
-//       url: '/api/users/signin',
-//       data: user
-//     })
-//     .then(function (resp) {
-//       return resp.data.token;
-//     });
-//   };
-
-
-
-
-
-// }]);
-
-// });
-
-// .factory('UserRequest', function ($http) {
-//   var getMatches = function () {
-//     return $http({
-//       method: 'POST',
-//       url: '/api/searchworks'
-//     })
-//     .then(function (resp) {
-//       return resp.data;
-//     });
-//   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-//http interceptor to work with :
-
-
-    // We add our $httpInterceptor into the array
-    // of interceptors. Think of it like middleware for your ajax calls
-//     $httpProvider.interceptors.push('AlreadyIncludedCheck');
-// })
-// .factory('AlreadyIncludedCheck', function ($window) {
-  // this is an $httpInterceptor
-  // its job is to stop all out going request
-  // then look in local storage and find the user's token
-  // then add it to the header so the server can validate the request
-  
-  // send it into lookupWork
-//   var checkDB = {
-//     request: function (object) {
-//       var gotIt = $window.localStorage.getItem('com.shortly');
-//       if (gotIt) {
-//         object.headers['x-access-token'] = gotIt;
-//       }
-//       object.headers['Allow-Control-Allow-Origin'] = '*';
-//       return object;
-//     }
-//   };
-//   return checkDB;
-// })
-
-
-
-
-
-// following controller code taken from Angular documentation on $http service
-// leaving here to fiddle with soon
-// app.controller('SiteController', function(){
-//  $http({
-//   method: 'GET',
-//   url: '/someUrl'
-// }).then(function successCallback(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-  // }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-//   });
 // })
