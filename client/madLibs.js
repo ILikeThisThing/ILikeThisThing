dc2a1cbba88af9b1f6ef916db637a87347f1c5a4
Brad Fincher <matrixv01@gmail.com>
2016-02-21 13:28:50 -0600

    meh

--- client/madLibs.js
+++ client/madLibs.js
@@ -10,40 +10,32 @@ angular.module('madLibs', [])
   };
 });
 
-$scope.saveData = function() {
-  $scope.data.push($scope.userInput);
-};
-
-$scope.submitForm = function() {
-  $http({
-    method: 'POST',
-    url: '/api/searchworks',
-    data: $scope.userInput,
-    headers: {
-      'Content-Type': 'application/x-www.form-urlencoded'
-    }
-  })
-
-  .success(function(data) {
-    if (data.errors) {
-      $scope.errorName = data.errors.name;
-      $scope.errorUserName = data.errors.username;
-      $scope.errorEmail = data.errors.email;
-    } else {
-      console.log("An error occured:", data.errors);
-    }
-  });
-};
-console.log($scope.userInput);
-})
-
-
-
-
-
-
-
-
+// $scope.saveData = function() {
+//   $scope.data.push($scope.userInput);
+// };
+//
+// $scope.submitForm = function() {
+//   $http({
+//     method: 'POST',
+//     url: '/api/searchworks',
+//     data: $scope.userInput,
+//     headers: {
+//       'Content-Type': 'application/x-www.form-urlencoded'
+//     }
+//   })
+//
+//   .success(function(data) {
+//     if (data.errors) {
+//       $scope.errorName = data.errors.name;
+//       $scope.errorUserName = data.errors.username;
+//       $scope.errorEmail = data.errors.email;
+//     } else {
+//       console.log("An error occured:", data.errors);
+//     }
+//   });
+// };
+// console.log($scope.userInput);
+// })
 
 // .factory('Tags', function ($http) {
 //   var getAll = function () {
