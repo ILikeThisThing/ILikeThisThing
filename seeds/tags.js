c607882071538d64b44c1397762d2bbc835ecb49
Julia Brown <juliacelestebrown@gmail.com>
2016-02-21 14:55:24 -0600

    added LOADS of tags

--- seeds/tags.js
+++ seeds/tags.js
@@ -6,13 +6,21 @@ exports.seed = function(knex, Promise) {
   //   knex('WorkTag').del(), 
 
     // Inserts seed entries
-    knex('Tags').insert([{tag: 'Dark/macabre'}, {tag: 'Absurdist'}, {tag: 'Existential'},
-    	{tag: 'Happy-Go-Lucky'}, {tag: 'Dystopian'}, {tag: 'Utopian'}, {tag: 'Beauty/Wonderment'},
-    	{tag: 'Weird'}, {tag: 'Goofy'}, {tag: 'Stream of Consciousness'}, {tag: 'Vignette'},
-    	{tag: 'Present Day'}, {tag: 'Victorian era'}, {tag: 'Medieval times'}, {tag: 'Prehistoric times'}, {tag: 'Action'},
-    	{tag: 'Adventure'}, {tag: 'Comedy'}, {tag: 'Crime'}, {tag: 'Fantasy'}, {tag: 'Horror'}, {tag: 'Musical'}, 
-    	{tag: 'Mystery'}, {tag: 'Political'}, {tag: 'Romance'}, {tag: 'Satire'}, {tag: 'Science Fiction'}, 
-    	{tag: 'Thriller'}, {tag: 'Western'}, {tag: 'So-Bad-its-Good'}, {tag: "Philosophy"}, {tag: "Programming"}])
+    knex('Tags').insert([{tag: 'Theme-Dark/macabre'}, {tag: 'Theme-Absurdist'}, {tag: 'Theme-Existential'},
+    	{tag: 'Theme-Happy-Go-Lucky'}, {tag: 'Theme-Dystopian'}, {tag: 'Theme-Utopian'}, {tag: 'Theme-Beauty/Wonderment'},
+    	{tag: 'Theme-Weird'}, {tag: 'Theme-Goofy'}, {tag: 'Writing Style-Stream of Consciousness'}, {tag: 'Writing Style-Vignette'},
+    	{tag: 'Time Period-Present Day'}, {tag: 'Time Period-Victorian era'}, {tag: 'Time Period-Medieval times'}, {tag: 'Time Period-Prehistoric times'}, {tag: 'Genre-Action'},
+    	{tag: 'Genre-Adventure'}, {tag: 'Genre-Comedy'}, {tag: 'Genre-Crime'}, {tag: 'Genre-Fantasy'}, {tag: 'Genre-Horror'}, {tag: 'Genre-Musical'}, 
+    	{tag: 'Genre-Mystery'}, {tag: 'Genre-Political'}, {tag: 'Genre-Romance'}, {tag: 'Genre-Satire'}, {tag: 'Genre-Science Fiction'}, 
+    	{tag: 'Genre-Thriller'}, {tag: 'Genre-Western'}, {tag: 'So-Bad-its-Good'}, {tag: "Theme-Philosophy"}, {tag: "Theme-Programming"}, {tag: "Time Period-Time Travel"},
+      {tag: 'Family-Friendly'}, {tag: 'Character Diversity'}, {tag: 'Gay/Lesbian'}, {tag: 'Gorey'}, {tag: 'Campy'}, {tag: 'Art Style-Full Color'},
+      {tag: 'Cult Classic'}, {tag: 'Art Style-Claymation'}, {tag: 'Art Style-Stop-Motion'}, {tag: 'Art Style-Pen and Ink'}, {tag: 'Art Style-CGI'},
+      {tag: 'Sound Design'}, {tag: 'Score'}, {tag: 'Plot-Fast Paced'}, {tag: 'Plot-Slow Paced'}, {tag: 'Plot-Plot Driven'}, {tag: 'Plot- Character driven'},
+      {tag:'World Building'}, {tag: 'Set Design-Elaborate'}, {tag: 'Set Design-Minimalist'}, {tag: 'Costuming-Period'}, {tag: 'Costuming-Modern'}, {tag: 'Costuming-Elaborate'}, 
+      {tag: 'Costuming-Minimalist'}, {tag: 'Cinematography'}, {tag: 'Setting-Outer Space'}, {tag: 'Setting-New York'}, {tag: 'Setting-Texas'}, {tag: 'Setting-Fantasy world'}, 
+      {tag: 'Setting-Space Station'}, {tag: 'Setting-Europe'}, {tag: 'Setting-Asia'}, {tag: 'Setting-North America'}, {tag: 'Setting-South America'}, {tag: 'Setting-Australia'}, {tag: 'Antarctica'},
+      {tag: 'Characters-Quirky'}, {tag: 'Characters-Smart'}, {tag: 'Characters-Funny'}, {tag: 'Characters-Hopelessly Incompetant'}, {tag: 'Characters-Pothead'}, {tag: 'Characters-Tortured'},
+      {tag: 'Characters-Romantic'}, {tag: 'Characters-Optimistic'}, {tag: 'Characters-Pessimistic'}])
   ).catch(function(err){
   	console.error('tags have already been added ', err.message)
   })
