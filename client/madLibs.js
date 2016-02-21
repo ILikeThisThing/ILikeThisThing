dc1bee52468ef2c8db98960b470196f4548e3851
Brad Fincher <matrixv01@gmail.com>
2016-02-21 10:50:16 -0600

    update madlibs,recommendations

--- client/madLibs.js
+++ client/madLibs.js
@@ -1,24 +1,58 @@
 angular.module('madLibs', [])
 
-.controller('RecController', function($scope, $http) {
+.controller('RecController', function($scope) {
   $scope.data = {};
-  $http.get('api/works')
-    .success(function(data) {
-      $scope.works = data;
-    })
-    .error(function(data) {
-      console.error(error);
-    });
-})
-
-.factory('Tags', function ($http) {
-  var getAll = function () {
-    return $http({
-      method: 'POST',
-      url: '/api/works'
-    })
-    .then(function (resp) {
-      return resp.data;
-    });
+  $scope.userInput = {
+    plot: '',
+    character: '',
+    setting: '',
+    genre: ''
   };
 });
+
+$scope.saveData = function() {
+  $scope.data.push($scope.userInput);
+};
+
+$scope.submitForm = function() {
+  $http({
+    method: 'POST',
+    url: '/api/searchworks',
+    data: $scope.userInput,
+    headers: {
+      'Content-Type': 'application/x-www.form-urlencoded'
+    }
+  })
+
+  .success(function(data) {
+    if (data.errors) {
+      $scope.errorName = data.errors.name;
+      $scope.errorUserName = data.errors.username;
+      $scope.errorEmail = data.errors.email;
+    } else {
+      console.log("An error occured:", data.errors);
+    }
+  });
+};
+console.log($scope.userInput);
+})
+
+
+
+
+
+
+
+
+
+// .factory('Tags', function ($http) {
+//   var getAll = function () {
+//     return $http({
+//       method: 'POST',
+//       url: '/api/works'
+//     })
+//     .then(function (resp) {
+//       return resp.data;
+//     });
+//   };
+// });
