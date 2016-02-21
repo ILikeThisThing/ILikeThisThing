ccfafd4609a13f0b261b3d30616cb590cb63a153
Brad Fincher <matrixv01@gmail.com>
2016-02-21 17:28:14 -0600

    added FACTORY

--- client/madLibs.js
+++ client/madLibs.js
@@ -1,21 +1,12 @@
-angular.module('madLibs', [])
+angular.module('MyApp', [])
 
-.controller('TagController', function($scope, $http) {
+.controller('TagController', function($scope, $http, ILikeThis) {
   $scope.themes = ['Theme-Dark/macabre', 'Theme-Absurdist', 'Theme-Existential'];
   $scope.characters = ['Characters-Quirky', 'Characters-Smart', 'Characters-Funny'];
   $scope.settings = ['Setting-Outer Space', 'Setting-New York', 'Setting-Texas'];
   $scope.genres = ['Genre-Action', 'Genre-Adventure', 'Genre-Comedy'];
+  $scope.submitForm = function(){
+    return ILikeThis.getMatchingTags($scope.userInput);
+  };
 });
-
-$scope.submitForm = function() {
-  $http({
-      method: 'POST',
-      url: '/api/tags',
-      data: $scope.userInput
-    })
-    .then(function successCallback(response) {
-      console.log(response);
-    }, function errorCallback(response) {
-      console.log(response);
-    });
 };
