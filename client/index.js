353c74953750140f543aeb4477ffe54f3be43a03
Natalie <natalie.s.sharpe@gmail.com>
2016-02-21 19:07:53 -0600

    transition to factories functional

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@
 var app=angular.module('ILikeThis.homepage', []);
 
 
-app.controller('RequestController', function($scope, ILikeThis) {
+app.controller('RequestController', function($scope, Factory) {
 
 //different types to populate the dropdown menu
 $scope.types = ['Books', 'Movies', 'Games'];
@@ -14,7 +14,7 @@ $scope.userInput = {
 };
 
 $scope.submitForm = function() {
-  ILikeThis.submitForm()
+  Factory.submitForm($scope.userInput)
     .then(function successCallback(response) {
       // this callback will be called asynchronously
       // when the response is available
@@ -26,54 +26,53 @@ $scope.submitForm = function() {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
     });
-    return data;
     }
   });
 
 
-var helperFunction = function() {
-  if ($scope.data[0].type === 'Movies') {
-      for (i = 0; i < $scope.data.length; i ++) {
-        if ($scope.data[i].database) {
-          $scope.data[i].title = title;
-          $scope.data[i].poster = image;
-          $scope.data[i].director = person;
+// var helperFunction = function() {
+//   if ($scope.data[0].type === 'Movies') {
+//       for (i = 0; i < $scope.data.length; i ++) {
+//         if ($scope.data[i].database) {
+//           $scope.data[i].title = title;
+//           $scope.data[i].poster = image;
+//           $scope.data[i].director = person;
 
-        } else //if (!$scope.data[i].database) {
-          $scope.data[i].title = title;
-          $scope.data[i].poster = image;
-          $scope.data[i].director = person;
-      }}
+//         } else //if (!$scope.data[i].database) {
+//           $scope.data[i].title = title;
+//           $scope.data[i].poster = image;
+//           $scope.data[i].director = person;
+//       }}
 
 
-    } else if($scope.data[0].type === 'Books') {
-      for (i = 0; i < $scope.data.length; i ++) {
-        if ($scope.data[i].database) {
-          $scope.data[i].title = title;
-          $scope.data[i].largeImage = image;
-          $scope.data[i].authors[0] = person; //need to deal with instances where multiple authors
+//     } else if($scope.data[0].type === 'Books') {
+//       for (i = 0; i < $scope.data.length; i ++) {
+//         if ($scope.data[i].database) {
+//           $scope.data[i].title = title;
+//           $scope.data[i].largeImage = image;
+//           $scope.data[i].authors[0] = person; //need to deal with instances where multiple authors
         
-        } else //if (!$scope.data[i].database) {
-            $scope.data[i].title = title;
-            $scope.data[i].largeImage = image;
-            $scope.data[i].authors[0] = person; //need to deal with instances where multiple authors
-          }
+//         } else //if (!$scope.data[i].database) {
+//             $scope.data[i].title = title;
+//             $scope.data[i].largeImage = image;
+//             $scope.data[i].authors[0] = person; //need to deal with instances where multiple authors
+//           }
 
 
-    } else if ($scope.data[0].type === 'Games') {
-      for (i = 0; i < $scope.data.length; i ++) {
-        if ($scope.data[i].database) {
-          $scope.data[i].name = title;
-          $scope.data[i].largeImage = image;
-          $scope.data[i].original_release_date = date
+//     } else if ($scope.data[0].type === 'Games') {
+//       for (i = 0; i < $scope.data.length; i ++) {
+//         if ($scope.data[i].database) {
+//           $scope.data[i].name = title;
+//           $scope.data[i].largeImage = image;
+//           $scope.data[i].original_release_date = date
         
-        } else //if (!$scope.data[i].database) {
-        $scope.data[i].name = title;
-        $scope.data[i].image = image;
-        $scope.data[i].original_release_date = date
-      }
-    }
-  };
+//         } else //if (!$scope.data[i].database) {
+//         $scope.data[i].name = title;
+//         $scope.data[i].image = image;
+//         $scope.data[i].original_release_date = date
+//       }
+//     }
+//   };
 
 //make each image or title selectable and on click send them to that results page...
 
