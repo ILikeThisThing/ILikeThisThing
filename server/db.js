b6199b0f3d7685006c303ea3d12ff85dac85ea41
Julia Brown <juliacelestebrown@gmail.com>
2016-02-22 17:25:52 -0600

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,9 +4,13 @@ var knex        = require('knex')(config[env]);
 
 module.exports = knex;
 
-
+//if there are any new migrations it will run this
+//if changes need to be made to the database a new migration must be made (knex migrate:make <db name>)
+//it will then apply those changes ON TOP of the last migration 
 knex.migrate.latest()
 .then(function(){
+	//it will run the contents of the seeds file EVERY TIME node restarts
+	//-- the tags table is all unique to account for this
 	knex.seed.run(); 
 });
 
